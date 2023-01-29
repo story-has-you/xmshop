b25d7c9fdf1fd977c71b76e41ad10244f742888a
fangxi <loveinori@qq.com>
2023-01-29 17:07:38 +0800

    tabs显隐

--- lib/app/modules/product_detail/controllers/product_detail_controller.dart
+++ lib/app/modules/product_detail/controllers/product_detail_controller.dart
@@ -3,15 +3,38 @@ import 'package:get/get.dart';
 
 class ProductDetailController extends GetxController {
   final ScrollController scrollController = ScrollController();
-
   final RxDouble opcity = 0.0.obs;
+  final List<dynamic> tabs = [
+    {"id": 1, "title": "商品"},
+    {"id": 2, "title": "详情"},
+    {"id": 3, "title": "推荐"},
+  ];
+
+  final RxInt currentTabIndex = 1.obs;
+  final RxBool showTab = false.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _initScroll();
+    update();
+  }
+
+  void setCurrentTabIndex(int index) {
+    currentTabIndex.value = index;
+    update();
+  }
 
   _initScroll() {
     scrollController.addListener(() {
       double pixels = scrollController.position.pixels;
       if (pixels <= 100) {
         opcity.value = pixels / 100;
-        update();
+        if (showTab.value) {
+          showTab.value = false;
+        }
+      } else if (!showTab.value) {
+        showTab.value = true;
       }
     });
   }
--- lib/app/modules/product_detail/views/product_detail_view.dart
+++ lib/app/modules/product_detail/views/product_detail_view.dart
@@ -12,7 +12,7 @@ class ProductDetailView extends GetView<ProductDetailController> {
       // 让内容区域扩展到appBar, 实现透明导航
       extendBodyBehindAppBar: true,
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(ScreenAdapter.height(96)),
+        preferredSize: Size.fromHeight(ScreenAdapter.height(120)),
         child: Obx(() => _appBar()),
       ),
       body: Center(
@@ -47,60 +47,33 @@ class ProductDetailView extends GetView<ProductDetailController> {
         ),
       ),
       title: SizedBox(
-        width: ScreenAdapter.width(400),
-        height: ScreenAdapter.height(96),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("商品", style: TextStyle(fontSize: ScreenAdapter.fontSize(36))),
-                  Container(
-                    margin: EdgeInsets.only(top: ScreenAdapter.height(10)),
-                    width: ScreenAdapter.width(100),
-                    height: ScreenAdapter.height(2),
-                    color: Colors.red,
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("详情", style: TextStyle(fontSize: ScreenAdapter.fontSize(36))),
-                  Container(
-                    margin: EdgeInsets.only(top: ScreenAdapter.height(10)),
-                    width: ScreenAdapter.width(100),
-                    height: ScreenAdapter.height(2),
-                    color: Colors.red,
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("推荐", style: TextStyle(fontSize: ScreenAdapter.fontSize(36))),
-                  Container(
-                    margin: EdgeInsets.only(top: ScreenAdapter.height(10)),
-                    width: ScreenAdapter.width(100),
-                    height: ScreenAdapter.height(2),
-                    color: Colors.red,
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+          width: ScreenAdapter.width(400),
+          height: ScreenAdapter.height(96),
+          child: controller.showTab.value
+              ? Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: controller.tabs.map((e) {
+                    var id = e['id'];
+                    var title = e['title'];
+                    return InkWell(
+                      onTap: () => controller.setCurrentTabIndex(id),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(title, style: TextStyle(fontSize: ScreenAdapter.fontSize(36))),
+                          Container(
+                            margin: EdgeInsets.only(top: ScreenAdapter.height(10)),
+                            width: ScreenAdapter.width(100),
+                            height: ScreenAdapter.height(2),
+                            color: id == controller.currentTabIndex.value ? Colors.red : Colors.transparent,
+                          ),
+                        ],
+                      ),
+                    );
+                  }).toList(),
+                )
+              : const Text("")),
       centerTitle: true,
       backgroundColor: Colors.white.withOpacity(controller.opcity.value),
       elevation: 0,
