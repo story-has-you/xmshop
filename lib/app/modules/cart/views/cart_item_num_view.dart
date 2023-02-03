1444e3ec9446de5ceee93d099b2fc4aaac383f53
fangxi <loveinori@qq.com>
2023-02-03 14:09:45 +0800

    购物车页面布局。

---
+++ lib/app/modules/cart/views/cart_item_num_view.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:xmshop/app/services/screen_adapter.dart';
+
+class CartItemNumView extends GetView {
+  const CartItemNumView({Key? key}) : super(key: key);
+  Widget _left() {
+    return Container(
+      alignment: Alignment.center,
+      width: ScreenAdapter.width(80),
+      height: ScreenAdapter.height(64),
+      child: const Text("-"),
+    );
+  }
+
+  Widget _center() {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border(
+        left: BorderSide(width: ScreenAdapter.width(2), color: Colors.black12),
+        right: BorderSide(width: ScreenAdapter.width(2), color: Colors.black12),
+      )),
+      alignment: Alignment.center,
+      width: ScreenAdapter.width(80),
+      height: ScreenAdapter.height(64),
+      child: const Text("0"),
+    );
+  }
+
+  Widget _right() {
+    return Container(
+      alignment: Alignment.center,
+      width: ScreenAdapter.width(80),
+      height: ScreenAdapter.height(64),
+      child: const Text("+"),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: ScreenAdapter.width(244),
+      height: ScreenAdapter.height(60),
+      decoration: BoxDecoration(border: Border.all(width: ScreenAdapter.width(2), color: Colors.black12)),
+      child: Row(
+        children: [_left(), _center(), _right()],
+      ),
+    );
+  }
+}
--- lib/app/modules/cart/views/cart_view.dart
+++ lib/app/modules/cart/views/cart_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:xmshop/app/modules/cart/controllers/cart_controller.dart';
+import 'package:xmshop/app/modules/cart/views/cart_item_view.dart';
 import 'package:xmshop/app/services/screen_adapter.dart';
 
 class CartView extends GetView<CartController> {
@@ -10,60 +11,61 @@ class CartView extends GetView<CartController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('CartView'),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: const Text('购物车'),
         centerTitle: true,
+        actions: [TextButton(onPressed: () {}, child: const Text("编辑"))],
       ),
       body: Stack(
         children: [
           ListView(
-            children: [],
+            children: const [
+              CartItemView(),
+              CartItemView(),
+              CartItemView(),
+              CartItemView(),
+              CartItemView(),
+              CartItemView(),
+              CartItemView(),
+              CartItemView(),
+            ],
           ),
           Positioned(
-            left: 0,
-            right: 0,
-            bottom: 0,
-            child: Container(
-              padding: EdgeInsets.only(left: ScreenAdapter.width(20), right: ScreenAdapter.width(20)),
-              width: double.infinity,
-              height: ScreenAdapter.height(190),
-              decoration: BoxDecoration(
-                border: Border(
-                  top: BorderSide(
-                    color: Colors.black12,
-                    width: ScreenAdapter.height(2),
-                  ),
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Container(
+                padding: EdgeInsets.only(right: ScreenAdapter.width(20)),
+                width: ScreenAdapter.width(1080),
+                height: ScreenAdapter.height(190),
+                decoration: BoxDecoration(border: Border(top: BorderSide(color: const Color.fromARGB(178, 240, 236, 236), width: ScreenAdapter.height(2))), color: Colors.white),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Checkbox(
+                            value: true,
+                            onChanged: (value) {
+                              print(value);
+                            }),
+                        const Text("全选")
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Text("合计: "),
+                        Text("¥98.9", style: TextStyle(fontSize: ScreenAdapter.fontSize(58), color: Colors.red)),
+                        SizedBox(width: ScreenAdapter.width(20)),
+                        ElevatedButton(style: ButtonStyle(backgroundColor: MaterialStateProperty.all(const Color.fromRGBO(255, 165, 0, 0.9)), foregroundColor: MaterialStateProperty.all(Colors.white), shape: MaterialStateProperty.all(
+                            // CircleBorder()
+                            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)))), onPressed: () {}, child: Text("结算"))
+                      ],
+                    )
+                  ],
                 ),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Checkbox(value: true, onChanged: (value) {}),
-                      const Text("全选"),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      const Text("合计:"),
-                      Text(
-                        "111",
-                        style: TextStyle(fontSize: ScreenAdapter.fontSize(38), color: Colors.red),
-                      ),
-                      SizedBox(width: ScreenAdapter.width(20)),
-                      ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(const Color.fromRGBO(255, 165, 0, 0.9)),
-                        ),
-                        onPressed: () {},
-                        child: const Text("结算"),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          )
+              ))
         ],
       ),
     );
